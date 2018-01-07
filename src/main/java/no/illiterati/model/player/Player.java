f1dff96ea216b47bc8d56c42beed339457a3c623
yan007 <yan007@student.uib.no>
2018-01-07 16:13:05 +0100

    altered Player.java

--- src/main/java/no/illiterati/model/player/Player.java
+++ src/main/java/no/illiterati/model/player/Player.java
@@ -1,5 +1,12 @@
 package no.illiterati.model.player;
 
-public interface Player {
+import java.util.Collection;
 
+import no.illiterati.model.actor.Actor;
+
+public interface Player {
+	public String go();
+	public String where();
+	public Collection<Actor> inventory();
+	public String lookInventory();
 }
